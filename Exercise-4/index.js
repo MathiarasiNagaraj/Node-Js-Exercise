e6e8986ed2e462c61b4f56fb92c3e9dacb275f74
MathiarasiNagaraj <125631817+MathiarasiNagaraj@users.noreply.github.com>
2023-10-30 13:02:56 +0530

    read and write created

--- Exercise-4/index.js
+++ Exercise-4/index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-require('dotenv').config();
+const dotenv = require('dotenv');
+dotenv.config();
 const PORT = process.env.PORT
 app.listen(PORT, () => { console.log(`server listning to ${PORT}`) });
-
-
 app.get(`/index(.html)?`, (req, res) => {
     res.sendFile(path.join(__dirname,'views', 'index.html'));
 })
---
+++ Exercise-4/utils/fileUtils.js
@@ -0,0 +1,19 @@
+const { readFile,writeFile} = require('fs');
+const { join } = require('path');
+
+
+const readBuddiesFile = async (path) => {
+    
+    
+}
+const writeBuddiesFile = async (path) => {
+    
+}
+
+
+
+
+module.exports = {
+    readBuddiesFile,
+    writeBuddiesFile
+}
