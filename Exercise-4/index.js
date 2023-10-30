3d5f4a9489de9fdbf986c0e0e5d52e7436747b98
MathiarasiNagaraj <125631817+MathiarasiNagaraj@users.noreply.github.com>
2023-10-30 12:47:48 +0530

    --

---
+++ Exercise-4/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+require('dotenv').config();
+const PORT = process.env.PORT
+app.listen(PORT, () => { console.log(`server listning to ${PORT}`) });
+
+
+app.get(`/index(.html)?`, (req, res) => {
+    res.sendFile(path.join(__dirname,'views', 'index.html'));
+})
+app.get(`^/$`, (req, res) => {
+    res.sendFile(path.join(__dirname,'views', 'details.html'));
+})
